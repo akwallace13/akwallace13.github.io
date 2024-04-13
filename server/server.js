a2319bbdfbb8ab7d86b8b75567818695ca0c859f
akwallace13 <132770092+akwallace13@users.noreply.github.com>
2024-04-13 13:06:50 -0500

    modified:   client/src/components/createaccount.html 	modified:   client/src/components/userlogin.html 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -6,18 +6,73 @@ const pool = require('./db.js')
 
 app.use(cors())
 
-
-
 //get puzzle(s)
-app.get('/food_and_drink', async (req, res) => {
+// app.get('/food', async (req, res) => {
+//     //const userEmail = 'test@test.com'
+//     //res.send('food and drink test')
+//     try {
+//     const food_and_drink_puzzles = await pool.query('SELECT * FROM food_and_drink_puzzles')
+//     res.json(food_and_drink_puzzles.rows)
+//     } catch (err) {
+//         console.error(error)
+//     }
+// })
+
+// app.get('/job', async (req, res) => {
+//     //const userEmail = 'test@test.com'
+//     //res.send('food and drink test')
+//     try {
+//     const jobs_puzzles = await pool.query('SELECT * FROM jobs_puzzles')
+//     res.json(jobs_puzzles.rows)
+//     } catch (err) {
+//         console.error(error)
+//     }
+// })
+
+// app.get('/phrase', async (req, res) => {
+//     //const userEmail = 'test@test.com'
+//     //res.send('food and drink test')
+//     try {
+//     const phrase_puzzles = await pool.query('SELECT * FROM phrase_puzzles')
+//     res.json(phrase_puzzles.rows)
+//     } catch (err) {
+//         console.error(error)
+//     }
+// })
+
+// app.get('/place', async (req, res) => {
+//     //const userEmail = 'test@test.com'
+//     //res.send('food and drink test')
+//     try {
+//     const place_puzzles = await pool.query('SELECT * FROM place_puzzles')
+//     res.json(place_puzzles.rows)
+//     } catch (err) {
+//         console.error(error)
+//     }
+// })
+
+// app.get('/random', async (req, res) => {
+//     //const userEmail = 'test@test.com'
+//     //res.send('food and drink test')
+//     try {
+//     const random_puzzles = await pool.query('SELECT * FROM random_puzzles')
+//     res.json(random_puzzles.rows)
+//     } catch (err) {
+//         console.error(error)
+//     }
+// })
+
+app.get('/WordGame', async (req, res) => {
+    //res.send('test')
     //const userEmail = 'test@test.com'
     //res.send('food and drink test')
     try {
-    const food_and_drink_puzzles = await pool.query('SELECT * FROM food_and_drink_puzzles')
-    res.json(food_and_drink_puzzles.rows)
+        //await
+    const word_puzzles = await pool.query('SELECT * FROM word_puzzles')
+    res.json(word_puzzles.rows)
     } catch (err) {
         console.error(error)
-    }
+    } 
 })
 
 app.listen(PORT, ( )=> console.log(`Server running on PORT ${PORT}`))
