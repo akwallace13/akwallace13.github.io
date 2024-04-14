7467273058b5f69e333232d00bc7633e41bf2c78
akwallace13 <132770092+akwallace13@users.noreply.github.com>
2024-04-14 17:32:31 -0500

    new file:   client/.env 	modified:   client/src/components/Auth.js 	modified:   client/src/components/Games.css 	modified:   client/src/components/userlogin.html

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,9 +1,34 @@
+import { useState } from 'react'
+
 const Auth = () => {
-    return (
-      <div>
-        found on app.js
+  const [isLogIn, setisLogin] = useState(true)
+  const [error, setError] = useState(null)
+  //const isLogIn = false
+
+  const viewLogin = (status) => {
+    setError(null)
+    setisLogin(status)
+  }
+
+  return (
+    <div className="auth-container">
+      <div className="auth-container-box">
+        <form>
+          <h2>{isLogIn ? 'Please log in' : 'Please sign up'}</h2>
+          <input type="email" placeholder="email"/>
+          <input type="password" placeholder="password"/>
+          {!isLogIn && <input type="password" placeholder="confirm password"/>}
+          <input type="submit" className="create"/>
+          {error && <p>{error}</p>}
+        </form>
+        <div className='auth-options'>
+          <button onClick={() => viewLogin(false)} style={{backgroundColor : !isLogIn ? 'turquoise' : 'white'}}>Sign Up</button>
+          <button onClick={() => viewLogin(true)} style={{backgroundColor : isLogIn ? 'turquoise'
+          : 'white'}}>Login</button>
+        </div>
       </div>
-    );
+    </div>
+  );
   }
   
   export default Auth;
